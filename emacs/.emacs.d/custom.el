ff36a2159ff4110287f74cac72ba838376aa2190
Byungkuk Choi <litlpoet@gmail.com>
2015-11-05 02:12:19 +0900

    Tested on windows. magit config added.

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -42,6 +42,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-diff-file-heading ((t (:inverse-video t :weight extra-bold)))))
+ )
 
 ;;; custom.el ends here
