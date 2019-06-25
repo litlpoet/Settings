8894e91299049ba9ab5e5df5bba383ca62c2ff9d
Byungkuk Choi <litlpoet@gmail.com>
2019-06-26 08:41:17 +1200

    lsp python changed.

--- emacs/elisps/init-languages-python.el
+++ emacs/elisps/init-languages-python.el
@@ -11,20 +11,34 @@
 ;;   :init
 ;;   (setq venv-location "/home/bk/dev/pyvenvs/"))
 
-(use-package lsp-python
-  :commands (lsp-python-enable)         ; not in the lsp-python autoloads
+;; (use-package lsp-python
+;;   :commands (lsp-python-enable)         ; not in the lsp-python autoloads
+;;   :hook (python-mode
+;;          . (lambda()
+;;              (progn
+;;                (condition-case nil
+;;                    (lsp-python-enable)
+;;                  (user-error nil))
+;;                (setq-local company-idle-delay 0.3)
+;;                (setq-local company-backends
+;;                            '((company-lsp company-yasnippet)
+;;                              company-capf company-files)))))
+;;   :config
+;;   (setq lsp-python-use-init-for-project-root t))
+
+(use-package lsp-python-ms
   :hook (python-mode
-         . (lambda()
-             (progn
-               (condition-case nil
-                   (lsp-python-enable)
-                 (user-error nil))
-               (setq-local company-idle-delay 0.3)
-               (setq-local company-backends
-                           '((company-lsp company-yasnippet)
-                             company-capf company-files)))))
+         . (lambda() (progn
+                  (require 'lsp-python-ms)
+                  (condition-case nil
+                      (lsp)
+                    (user-error nil))
+                  (setq-local company-idle-delay 0.3))))
   :config
-  (setq lsp-python-use-init-for-project-root t))
+  (setq lsp-python-ms-dir
+        (expand-file-name "/usr/lib/microsoft-python-language-server/"))
+  (setq lsp-python-ms-executable
+        "/usr/bin/mspyls"))
 
 (provide 'init-languages-python)
 ;;; init-languages-python.el ends here
